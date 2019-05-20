19fb36df3b142c8b735ffe37d81bf91b1e7929e8
Chris Shaffer <chris@methodbox.io>
2019-05-19 22:22:34 -0500

    working POC backend proxy api for google directions api

--- app.js
+++ app.js
@@ -1,58 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// node modules
-const path = require("path");
-/* add other modules here */
-const express = require("express");
-const request = require("request-promise-native");
-/* variables */
+const express = require('express');
+const fetch = require('node-fetch');
+const queryString = require('querystring');
 const app = express();
-const port = process.env.PORT || 5000;
-let testVariable = 'bob';
-app.set('view engine', 'ejs');
-app.use('/', express.static(path.join(__dirname + '/public')));
-/* run as static -comment this out, make form action /submit instead of '/'*/
+const apiKey = '';
+
 app.get('/', (req, res) => {
-    console.log(req);
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-app.get('/contact', (req, res) => {
-    console.log(req);
-    res.sendFile(path.join(__dirname + '/public/contact.html'));
-});
-app.listen(port, () => {
-    console.log(`Listening on ${port}`);
-});
-app.post('/submit', (req, res) => {
-    console.log(req.body);
-    const requestOptions = {
-        /**
-         * @requestOptions
-         * method: some method (POST, GET, etc.),
-         * uri: 'request uri',
-         * form: {
-         *   some json describing form data
-         * },
-         * json: true //  specify response type
-         */
-        method: 'POST',
-        form: {
-            secret: 'googleApiKey',
-        },
-        json: true,
-        url: '',
-    };
-    request(requestOptions)
-        .then(res => {
-        return res.success;
-    })
-        .then(success => {
-        if (!success) {
-            res.json({ success: false, error: 'some err...' });
-        }
-        else if (success) {
-            //  successful api request
-        }
+  req.query.key = apiKey;
+  const encodedQuery = queryString.encode(req.query);
+
+  fetch(`https://maps.googleapis.com/maps/api/directions/json?${encodedQuery}${apiKey}`)
+    .then(res => res.json())
+    .then(json => {
+      return res.send(json);
     });
 });
-//# sourceMappingURL=app.js.map
+
+app.listen(5000);
--- app.ts
+++ app.ts
@@ -1,58 +1,11 @@
-// node modules
-import * as path from 'path';
-/* add other modules here */
 import * as express from 'express';
-import * as request from 'request-promise-native';
-/* variables */
-const app = express();
-const port = process.env.PORT || 5000;
+import bodyParser = require('body-parser');
+import url = require('url');
+import querystring = require('querystring');
+const Article = require('./models').Article;
 
-app.set('view engine', 'ejs');
-app.use('/', express.static(path.join(__dirname + '/public')));
-
-/* run as static -comment this out, make form action /submit instead of '/'*/
 app.get('/', (req, res) => {
-  console.log(req);
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  console.log(req.query);
 });
 
-app.get('/contact', (req, res) => {
-  console.log(req);
-  res.sendFile(path.join(__dirname + '/public/contact.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
-
-app.post('/submit', (req, res) => {
-  console.log(req.body);
-  const requestOptions = {
-    /**
-     * @requestOptions
-     * method: some method (POST, GET, etc.),
-     * uri: 'request uri',
-     * form: {
-     *   some json describing form data
-     * },
-     * json: true //  specify response type
-     */
-    method: 'POST',
-    form: {
-      secret: 'googleApiKey',
-    },
-    json: true,
-    url: '',
-  };
-  request(requestOptions)
-    .then(res => {
-      return res.success;
-    })
-    .then(success => {
-      if (!success) {
-        res.json({ success: false, error: 'some err...' });
-      } else if (success) {
-        //  successful api request
-      }
-    });
-});
+app.listen(8080);
