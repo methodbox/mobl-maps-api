92829e7d34a421b7ba35a3ed5731d1d9ba56a035
Chris Shaffer <chris@methodbox.io>
2019-05-23 22:38:48 -0500

    moved api into get block, renamed const stringQuery to query

--- app.ts
+++ app.ts
@@ -4,15 +4,14 @@ import * as queryString from 'querystring';
 import fetch from 'node-fetch';
 
 const app: Express = express();
-const apiKey: string = '';
 
 app.get(
   '/',
   (req: Request, res: Response): void => {
-    req.query.key = apiKey;
-    const stringQuery: string = queryString.stringify(req.query);
+    const apiKey: string = req.query.key;
+    const query: string = queryString.stringify(req.query);
 
-    fetch(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}${apiKey}`)
+    fetch(`https://maps.googleapis.com/maps/api/directions/json?${query}${apiKey}`)
       .then(mapRes => mapRes.json())
       .then(json => {
         return res.send(json);
