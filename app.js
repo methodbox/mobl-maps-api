4570bd3413f288355b008dde2c92a68125757407
Chris Shaffer <chris@methodbox.io>
2019-05-24 20:22:38 -0500

    added formatting configs, tslint and prettier plugins

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const node_fetch_1 = require("node-fetch");
 const app = express();
 const apiKey = '';
 app.get('/', (req, res) => {
-    const queryObj = {
-        origin: req.query.origin,
-        destination: req.query.destination,
-        key: apiKey,
-        mode: req.query.mode,
-        language: req.query.language,
-        region: req.query.region,
-    };
-    const stringQuery = queryString.stringify(queryObj);
-    node_fetch_1.default(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}`)
-        .then(mapRes => mapRes.json())
-        .then(json => {
-        return res.send(json);
-    })
-        .catch(err => console.log(err));
+	const queryObj = {
+		origin: req.query.origin,
+		destination: req.query.destination,
+		key: apiKey,
+		mode: req.query.mode,
+		language: req.query.language,
+		region: req.query.region,
+	};
+	const stringQuery = queryString.stringify(queryObj);
+	node_fetch_1.default(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}`)
+		.then(mapRes => mapRes.json())
+		.then(json => {
+			return res.send(json);
+		})
+		.catch(err => console.log(err));
 });
 app.listen(5000);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -6,26 +6,23 @@ import fetch from 'node-fetch';
 const app: Express = express();
 const apiKey: string = '';
 
-app.get(
-  '/',
-  (req: Request, res: Response): void => {
-    const queryObj = {
-      origin: req.query.origin,
-      destination: req.query.destination,
-      key: apiKey,
-      mode: req.query.mode,
-      language: req.query.language,
-      region: req.query.region,
-    };
-    const stringQuery: string = queryString.stringify(queryObj);
+app.get('/', (req: Request, res: Response): void => {
+	const queryObj = {
+		origin: req.query.origin,
+		destination: req.query.destination,
+		key: apiKey,
+		mode: req.query.mode,
+		language: req.query.language,
+		region: req.query.region,
+	};
+	const stringQuery: string = queryString.stringify(queryObj);
 
-    fetch(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}`)
-      .then(mapRes => mapRes.json())
-      .then(json => {
-        return res.send(json);
-      })
-      .catch(err => console.log(err));
-  }
-);
+	fetch(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}`)
+		.then(mapRes => mapRes.json())
+		.then(json => {
+			return res.send(json);
+		})
+		.catch(err => console.log(err));
+});
 
 app.listen(5000);
