e68abbd6132f381f236b7324ce98edff5cb9da12
Chris Shaffer <chris@methodbox.io>
2019-05-29 22:23:32 -0500

    added stronger typing

--- app.ts
+++ app.ts
@@ -6,23 +6,39 @@ import fetch from 'node-fetch';
 const app: Express = express();
 const apiKey: string = '';
 
-app.get('/', (req: Request, res: Response): void => {
-	const queryObj = {
-		origin: req.query.origin,
-		destination: req.query.destination,
-		key: apiKey,
-		mode: req.query.mode,
-		language: req.query.language,
-		region: req.query.region,
-	};
-	const stringQuery: string = queryString.stringify(queryObj);
+type QueryObj = {
+  origin: string;
+  destination: string;
+  key: string;
+  mode: string;
+  language: string;
+  region: string;
+};
 
-	fetch(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}`)
-		.then(mapRes => mapRes.json())
-		.then(json => {
-			return res.send(json);
-		})
-		.catch(err => console.log(err));
-});
+app.get(
+  '/',
+  (req: Request, res: Response): void => {
+    const queryObj: QueryObj = {
+      origin: req.query.origin,
+      destination: req.query.destination,
+      key: apiKey,
+      mode: req.query.mode,
+      language: req.query.language,
+      region: req.query.region,
+    };
+    const stringQuery: string = queryString.stringify(queryObj);
+
+    fetch(`https://maps.googleapis.com/maps/api/directions/json?${stringQuery}`)
+      .then(mapRes => {
+        return mapRes.json();
+      })
+      .then(
+        (json: Response): Response => {
+          return res.send(json);
+        }
+      )
+      .catch((err: Error) => console.log(err));
+  }
+);
 
 app.listen(5000);
